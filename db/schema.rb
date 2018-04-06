3d8408a33409bf4e9ecc8a0bec3a88ca71dc1e8a
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-04-06 20:00:47 +0530

    added associations between links and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406135941) do
+ActiveRecord::Schema.define(version: 20180406142808) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
