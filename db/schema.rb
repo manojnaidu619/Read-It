5c364efcdaf6539d89d9de01eb2cddb508a2e5c7
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-04-10 16:02:16 +0530

    Modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408163553) do
+ActiveRecord::Schema.define(version: 20180410102816) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
