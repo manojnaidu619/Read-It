0462a0aa5748951fd64ef9682b9f2b091b88e25c
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-04-12 18:51:36 +0530

    added footer

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 class Link < ApplicationRecord
   acts_as_votable
   belongs_to :user
-  has_many :comments
+  has_many :comments , dependent: :destroy
   validates :title, presence: true, uniqueness: true
   validates :url , presence: true, uniqueness: true
 end
